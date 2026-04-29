67462ddd3e57407fea640a5666de5cde50cec295
VanillaCold <35960636+VanillaCold@users.noreply.github.com>
2026-04-29 11:01:46 +0100

    sdl window for cursor

--- WiimoteMouse-Linux/Entrypoint.cpp
+++ WiimoteMouse-Linux/Entrypoint.cpp
@@ -143,17 +143,22 @@ int main(int argc, char **argv) {
         SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
         return 0;
     }
-    /*SDL_Window* win = SDL_CreateWindow("SDL3 Image",640, 480, 0);
+
+    SDL_Window* win = SDL_CreateWindow("SDL3 Image",512, 512, SDL_WINDOW_BORDERLESS | SDL_WINDOW_TRANSPARENT | SDL_WINDOW_NOT_FOCUSABLE | SDL_WINDOW_ALWAYS_ON_TOP | SDL_WINDOW_UTILITY );
     SDL_Renderer* ren = SDL_CreateRenderer(win, NULL);
     SDL_Surface* bmp = SDL_LoadPNG("MainCursor.png");
     SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, bmp);
+
+
+    SDL_SetWindowShape(win, bmp);
     SDL_DestroySurface(bmp);
 
     SDL_Event e;
 
     SDL_RenderClear(ren);
     SDL_RenderTexture(ren, tex, NULL, NULL);
-    SDL_RenderPresent(ren);*/
+    SDL_RenderPresent(ren);
+
 
 
     auto mouse = new WiimoteMouse();
